eb409742875554250b3616e5401e6a01a66f244f
aggounix <melaggouni@gmail.com>
2021-04-10 22:12:26 +0200

    Add main controller and connect it to caculator domain

--- RpnCalculator/Controllers/RpnController.cs
+++ RpnCalculator/Controllers/RpnController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RpnCalculator.Domain.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,46 @@ namespace RpnCalculator.Controllers
     [ApiController]
     public class RpnController : ControllerBase
     {
+        private static ICalculatorService CalculatorService;
+
+        private readonly ILogger<RpnController> _logger;
+
+        public RpnController(ILogger<RpnController> logger, ICalculatorService calculatorService)
+        {
+            _logger = logger;
+            CalculatorService = calculatorService;
+        }
+
+
+
+        [Route("PushItem")]
+        [HttpPost]
+        public void Push(double operand)
+        {
+            CalculatorService.Push(operand);
+        }
+
+
+        [Route("PushOperator")]
+        [HttpPost]
+        public void PushOperator(string op)
+        {
+            CalculatorService.Calculate(op);
+        }
+
+        [Route("GetStack")]
+        [HttpGet]
+        public string GetStack()
+        {
+            return CalculatorService.GetStack();
+        }
+
+
+        [Route("CleanStack")]
+        [HttpGet]
+        public void CleanStack()
+        {
+            CalculatorService.Clear();
+        }
     }
 }
--- RpnCalculator/Startup.cs
+++ RpnCalculator/Startup.cs
@@ -1,17 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RpnCalculator.Domain.Interface;
+using RpnCalculator.Domain.Model;
+using RpnCalculator.Domain.Service;
 using RpnCalculator.Errors;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RpnCalculator
 {
@@ -29,7 +25,9 @@ namespace RpnCalculator
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddSingleton<ICalculatorContainer, CalculatorContainer>();
+            services.AddTransient<ICalculatorExecutor, CalculatorExecutor>();
+            services.AddSingleton<ICalculatorService, CalculatorService>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
